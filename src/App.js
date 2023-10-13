488c899b97b49868f9792cccea6fce8d82ceaaf8
Ibukunoluwa <ademiluatise@gmail.com>
2023-10-13 19:45:46 +0100

    add input and display tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState('')
+  const [allTasks, setAllTasks] = useState([])
+  const [completedTasks, setCompletedTasks] = useState([])
+
+  const addTask = () => {
+      allTasks.push(task)
+      setAllTasks(allTasks)
+      setTask("")
+  }
+
+  // to change the value of the current task
+  const InputBarListener = (event) => {
+      setTask(event.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="addNewTask">
+        <input type="text" className="addTask" onChange={(event) => InputBarListener(event)}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <div className="tasks">
+        {
+          allTasks.map((task)=> {
+            return (
+              <p key={allTasks.indexOf(task)}>{task}</p>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
