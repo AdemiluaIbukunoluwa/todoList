6fa7783e6220fa074757e9a66b3f8ce702241b53
AdemiluaIbukun <ademiluatise@gmail.com>
2024-12-13 02:58:49 +0000

    slight ammendment

--- client/src/Components/CategoryTasks/Task.tsx
+++ client/src/Components/CategoryTasks/Task.tsx
@@ -4,7 +4,6 @@ import {Delete, Edit} from '@mui/icons-material';
 const Task = ({task, deleteHandler}) => {
   return (
     <div className='flex rounded px-4 py-2 border border-black mx-4 my-3 hover:bg-gray-100'>
-      <input type="checkbox" />
       <p>{task.taskName}</p>
       <div className='ml-auto flex'>
       <button className="editTask mr-3" >
